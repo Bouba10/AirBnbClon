79f15ee4afadb3b6a6db8605853ed4733af6680a
Bouba10 <72755006+Bouba10@users.noreply.github.com>
2023-11-08 18:30:50 +0100

    SearchAndFilterBar

---
+++ AirBnbClone/Core/Explore/SearchAndFilterBar.swift
@@ -0,0 +1,18 @@
+//
+//  SearchAndFilterBar.swift
+//  AirBnbClone
+//
+//  Created by Boubacar sidiki barry on 08.11.23.
+//
+
+import SwiftUI
+
+struct SearchAndFilterBar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SearchAndFilterBar()
+}
