426fd6099dfd219524acacec35397bd5fed962eb
Bouba10 <72755006+Bouba10@users.noreply.github.com>
2023-11-08 18:06:43 +0100

    explore View UI

---
+++ AirBnbClone/Core/Explore/ExploreView.swift
@@ -0,0 +1,18 @@
+//
+//  ExploreView.swift
+//  AirBnbClone
+//
+//  Created by Boubacar sidiki barry on 08.11.23.
+//
+
+import SwiftUI
+
+struct ExploreView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExploreView()
+}
