1479ed5a379833719951967253f0bdca67f43366
Bouba10 <72755006+Bouba10@users.noreply.github.com>
2023-11-08 16:41:43 +0100

    Initial Commit

---
+++ AirBnbClone/AirBnbCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirBnbCloneApp.swift
+//  AirBnbClone
+//
+//  Created by Boubacar sidiki barry on 08.11.23.
+//
+
+import SwiftUI
+
+@main
+struct AirBnbCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
